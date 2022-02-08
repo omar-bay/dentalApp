12730ba1117d0b4e3c5bf7034959d47046b2721c
omar-bay <omarmb2002@gmail.com>
2022-02-08 15:25:11 +0200

    handle data and prepare for TaskListScreen

---
+++ src/data/Services.ts
@@ -0,0 +1,24 @@
+const Services = [{
+    id: 0,
+    name: "Service of Honors",
+    tasks: [{
+        id: 3,
+        name: "task of Honors",
+        description: "open honors and do stuff."
+    }],
+},
+{
+    id: 1,
+    name: "Venom Teeth",
+    tasks: [{
+        id: 3,
+        name: "task of Honors",
+        description: "open honors and do stuff."
+    },{
+        id: 8,
+        name: "whitening",
+        description: "insert Tube and clean the upper lip. then go clean the down clip!"
+    }],
+}]
+
+export default Services
--- src/screens/PServicesScreen/index.tsx
+++ src/screens/PServicesScreen/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
 import ServiceCart from '../../components/ServiceCart'
 import styles from './styles'
+import Services from '../../data/Services'
 
 const PServicesScreen = () => {
     return (
         <View style={styles.root}>
             <ScrollView style={styles.lista} showsVerticalScrollIndicator={false}>
-                <ServiceCart/>
-                <ServiceCart/>
-                <ServiceCart/>
+                {
+                    Services.map((index) => (
+                        <ServiceCart />
+                    ))
+                }
             </ScrollView>
         </View>
     )

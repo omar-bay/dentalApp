c4f732bb6c661342d5aa8037c1f4f6d5b6b5f241
omar-bay <omarmb2002@gmail.com>
2022-03-04 00:09:35 +0200

    picker is delaying due to state you know

--- src/tests/TestPicker/index.tsx
+++ src/tests/TestPicker/index.tsx
@@ -1,33 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
-import IconFor from 'react-native-vector-icons/AntDesign'
-import {Picker} from '@react-native-picker/picker';
+// import { View, Text, StyleSheet } from 'react-native'
+// import React, { useState } from 'react'
+// import IconFor from 'react-native-vector-icons/AntDesign'
+// import {Picker} from '@react-native-picker/picker';
 
-const TestPicker = () => {
-    const [selectedLanguage, setSelectedLanguage] = useState();
+// const TestPicker = () => {
+//     const [selectedLanguage, setSelectedLanguage] = useState();
 
-    return (
-        <View style={styles.root}>
-            <Picker
-            selectedValue={selectedLanguage}
-            onValueChange={(itemValue, itemIndex) =>
-            setSelectedLanguage(itemValue)
-            }
-            dropdownIconColor="white"
-            numberOfLines={1}
-            >
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
-            </Picker>
-            <IconFor name="right" size={25}/>
-        </View>
-    )
-}
+//     return (
+//         <View style={styles.root}>
+//             <Picker
+//             selectedValue={selectedLanguage}
+//             onValueChange={(itemValue, itemIndex) =>
+//             setSelectedLanguage(itemValue)
+//             }
+//             dropdownIconColor="white"
+//             numberOfLines={1}
+//             >
+//                 <Picker.Item label="Java" value="java" />
+//                 <Picker.Item label="JavaScript" value="js" />
+//             </Picker>
+//             <IconFor name="right" size={25}/>
+//         </View>
+//     )
+// }
 
-const styles = StyleSheet.create({
-    root: {
-        backgroundColor: 'red'
-    }
-})
+// const styles = StyleSheet.create({
+//     root: {
+//         backgroundColor: 'red'
+//     }
+// })
 
-export default TestPicker
+// export default TestPicker

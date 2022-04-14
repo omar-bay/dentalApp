a215fa8e06e5ceed78d06d4e8125aac7ec96b9b1
omar-bay <omarmb2002@gmail.com>
2022-04-14 17:55:23 +0300

    our segment control

--- src/tests/SegmentControlTest/index.tsx
+++ src/tests/SegmentControlTest/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import SegmentControl from 'react-native-segment-control';
+// import SegmentControl from 'react-native-segment-control';
+import SegmentControl from '../../components/SegmentControl';
  
 const One = () => {
   return <Text style={styles.text}>This is first view</Text>;
 };
 const Two = () => {
   return <Text style={styles.text}>This is second view</Text>;
+};
+const Three = () => {
+  return <Text style={styles.text}>This is third view</Text>;
 };
 const segments = [
   {
@@ -16,12 +20,17 @@ const segments = [
   {
     title: 'Two',
     view: Two
+  }, {
+    title: 'Three',
+    view: Three
   }
 ];
 const App = () => {
   return (
     <View style={styles.container}>
-      <SegmentControl segments={segments} />
+      <SegmentControl
+      segments={segments}
+      />
     </View>
   );
 };

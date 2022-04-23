8bd6b8d02deb5b5a0a9dd86875a176d21fe0165a
omar-bay <omarmb2002@gmail.com>
2022-04-23 22:06:14 +0300

    pdf support

--- src/screens/HREmployeeScreen/Resume.tsx
+++ src/screens/HREmployeeScreen/Resume.tsx
@@ -1,10 +1,47 @@
-import { View, Text, Platform } from 'react-native'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
+import Pdf from 'react-native-pdf';
 import React from 'react'
 
 const Resume = () => {
+  const source = {
+    uri: 'http://www.africau.edu/images/default/sample.pdf',
+    cache: true 
+  };
+
   return (
-    <View><Text>resume</Text></View>
+      <View style={styles.container}>
+          <Pdf
+          source={source}
+          onLoadComplete={(numberOfPages,filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onPageChanged={(page,numberOfPages) => {
+            console.log(`Current page: ${page}`);
+          }}
+          onError={(error) => {
+            console.log(error);
+          }}
+          onPressLink={(uri) => {
+            console.log(`Link pressed: ${uri}`);
+          }}
+          style={styles.pdf}/>
+          <View style={{ height: 323 }}></View>
+      </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginTop: 3,
+  },
+  pdf: {
+      flex:1,
+      width:Dimensions.get('window').width,
+      height:Dimensions.get('window').height,
+  }
+});
 
 export default Resume

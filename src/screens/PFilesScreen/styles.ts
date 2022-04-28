95101ded425041284e793b17bd2a2962e8869c68
omar-bay <omarmb2002@gmail.com>
2022-04-28 22:46:10 +0300

    UI maintenance

--- src/screens/PFilesScreen/styles.ts
+++ src/screens/PFilesScreen/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
     root: {
         backgroundColor: '#e9f9fc',
-        marginVertical: 10,
+        paddingVertical: 5,
         flex: 1,
     },
     file_carts: {

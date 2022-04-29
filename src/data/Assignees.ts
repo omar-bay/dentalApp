4d21091fe6dee24ea077ea83f2f7ebc6ecd6c47e
omar-bay <omarmb2002@gmail.com>
2022-04-29 16:21:37 +0300

    Dums

--- src/data/Assignees.ts
+++ src/data/Assignees.ts
@@ -16,6 +16,22 @@ const Assignees = [
         SSN: 123456789,
         birth: new Date(),
         marital_status: "Married"
+    },
+    {
+        id: 1,
+        name: "Moe Janna",
+        email: "moe12@bau.com",
+        phone_num: "95 356 123",
+        profile_pic: "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg",
+        hr_type: HR_Type.Student,
+        home_num: "88 765 999",
+        office: "Main blg. 2nd floor.",
+        home_address: "Bchamoun 3rd route",
+        cv: "https://www.entnet.org/wp-content/uploads/2021/04/Instructions-for-Adding-Your-Logo-2.pdf",
+        nationality: "Lebanese",
+        SSN: 123456789,
+        birth: new Date(),
+        marital_status: "Single"
     }
 ]
 

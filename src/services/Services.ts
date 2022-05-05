28d992fcd9f7da9c3428b25d4ed84f1fbd4be0bb
omar-bay <omarmb2002@gmail.com>
2022-05-06 00:52:55 +0300

    modified:   src/services/Services.ts

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,4 +1,24 @@
-import { useState } from "react";
-import { Input } from "react-native-elements";
-import { CreateHrAssigneeDocument, CreateHrAssigneeMutation, useCreateHrAssigneeMutation } from "../../libs/generated/graphql";
+import { useCreateHrAssigneeMutation, Hr_Type } from '../../libs/generated/graphql'
+import { useMutation } from '@apollo/client'
 
+const example = `
+const [createHrAssignee] = useCreateHrAssigneeMutation()
+
+createHrAssignee({
+    variables: {
+        input: {
+            name: "Ali Zaart",
+            password: "123123",
+            email: "alizaart@bau.com",
+            phone_number: "70 834 321",
+            home_number: "01 788 993",
+            home_address: "Beirut, After Dinner",
+            SSN: "1234567890",
+            martial_status: "married",
+            nationality: "Lebanese",
+            profile_pic_url: "https://www.bau.edu.lb/BAUUpload/Staff/Images/Science/dr-ali-elzaart.jpg",
+            hr_type: Hr_Type.Doctor
+        }
+    }
+})
+`

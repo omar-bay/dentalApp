5e99f07a6b45e5f2321c67bdeb8c14ff7d3216bf
omar-bay <omarmb2002@gmail.com>
2022-05-05 15:06:10 +0300

    BACKEND: trying multiquery

--- src/screens/PFilesScreen/index.tsx
+++ src/screens/PFilesScreen/index.tsx
@@ -1,42 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, FlatList, ScrollView } from 'react-native'
 import FileCart from '../../components/FileCart'
 import styles from './styles'
 import PFiles from '../../data/PFiles'
 import { StackScreenProps } from '@react-navigation/stack'
 import { Patient, Nav, PFile } from '../Types'
-import { usePatientsQuery } from '../../../libs/generated/graphql'
 // import { gql, useQuery } from '@apollo/client'
 import axios from "axios";
 import { useQuery } from "react-query";
 import { DB_URL } from "../../global"
+import { createIconSetFromFontello } from 'react-native-vector-icons'
+
+const usePatientsQuery = () => {
+  // fetching patients data
+  const PATIENTS_QUERY = `
+  {
+    patients {
+      id
+      file_number
+      name
+      gender
+      dateOfBirth
+      cat_id
+      profile_pic_url
+      createdAt
+      updatedAt
+    }
+  }
+  `;
+  const {
+    data: patientsData,
+    isLoading: patientsIsLoading,
+    error: patientsError
+  } = useQuery("launches", () => {
+    return axios({
+      url: DB_URL,
+      method: "POST",
+      data: {
+        query: PATIENTS_QUERY
+      }
+    }).then(response => response.data.data);
+  });
+
+  return { patientsData, patientsIsLoading, patientsError };
+}
+
+const useFilesQuery = () => {
+  // fetching files data
+  const FILES_QUERY = `
+  {
+    files {
+      file_number
+      patient_id
+      status
+      assignee_id
+      createdAt
+      updatedAt
+    }
+  }
+  `;
+  const {
+    data: filesData,
+    isLoading: filesIsLoading,
+    error: filesError
+  } = useQuery("launches", () => {
+    return axios({
+      url: DB_URL,
+      method: "POST",
+      data: {
+        query: FILES_QUERY
+      }
+    }).then(response => response.data.data);
+  });
+
+  return { filesData, filesIsLoading, filesError };
+}
 
 const PFilesScreen = ({navigation, route}: Nav) => {
-    // fetching patients data
-    const PATIENTS_QUERY = `
-    {
-      patients {
-        id
-        file_number
-        name
-        gender
-        dateOfBirth
-        cat_id
-        profile_pic_url
-        createdAt
-        updatedAt
-      }
-    }
-  `;
-    const { data, isLoading, error } = useQuery("launches", () => {
-      return axios({
-        url: DB_URL,
-        method: "POST",
-        data: {
-          query: PATIENTS_QUERY
-        }
-      }).then(response => response.data.data);
-    });
+    const { patientsData, patientsIsLoading, patientsError } = usePatientsQuery();
+    const { filesData, filesIsLoading, filesError } = useFilesQuery();
+    console.log(filesData)
     
     return (
         <View style={styles.root}>
